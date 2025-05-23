f7d0373f7ce1d34cd6f4c65b276b1d1b665e0af5
Wil <disenokinocomp@gmail.com>
2025-05-23 09:23:42 -0500

    fix(api): export fetchAll alias en utils/api y/o renombrar import en DocumentList

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,10 @@
+// src/utils/api.js
+
+// 1) Listar
 export const listManifestos = () =>
   fetch('/api/manifestos').then(res => res.json());
 
+// 2) Crear
 export const createManifesto = data =>
   fetch('/api/manifestos', {
     method: 'POST',
@@ -8,6 +12,7 @@ export const createManifesto = data =>
     body: JSON.stringify(data),
   }).then(res => res.json());
 
+// 3) Actualizar
 export const updateManifesto = (id, data) =>
   fetch(`/api/manifestos/${id}`, {
     method: 'PUT',
@@ -15,5 +20,9 @@ export const updateManifesto = (id, data) =>
     body: JSON.stringify(data),
   }).then(res => res.json());
 
+// 4) Borrar
 export const deleteManifesto = id =>
   fetch(`/api/manifestos/${id}`, { method: 'DELETE' });
+
+// Alias para compatibilidad con fetchAll
+export { listManifestos as fetchAll };
